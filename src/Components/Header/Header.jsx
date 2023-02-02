4b84d90020220ff72743e2146fd38dfa21987651
v.symonovych <v.symonovych@4irelabs.com>
2023-02-02 22:45:11 +0200

    fixed logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import './Header.css';
 import logo from '../../Assets/Logo.svg';
 import { HeaderNav } from "../index";
+import {Link} from "react-router-dom";
 const Header = () => {
     return(
         <header className="header">
             <div className="header-container container">
-                <div className="header-logo"><img src={logo} alt="little lemon logo" /></div>
+                <div className="header-logo"><Link to="/"><img src={logo} alt="little lemon logo" /></Link></div>
                 <HeaderNav />
             </div>
         </header>
